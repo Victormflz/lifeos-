0f97ba00bc66223f169e192309ce476b2cd4292e
Víctor Flórez <victorproyectxs@gmail.com>
2026-04-03 14:41:36 +0200

    fix cors for vercel

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: 'https://lifeos-seven-alpha.vercel.app'
+}))
 app.use(express.json())
 
 // Conexión a MongoDB
